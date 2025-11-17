c40202033f2e0091d42607f65b223c9d7c02bd5a
Tanisha Lalwani <145191259+tanishaness@users.noreply.github.com>
2025-11-18 01:56:08 +0530

    01

--- template/src/App.jsx
+++ template/src/App.jsx
@@ -1,15 +1,20 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [a, setA] = useState(20) //read only a, write only setA and their value 20
-
+  const [a, setA] = useState(20) 
+  const [username, setUsername] = useState("Tanisha")
+  const [users, setUsers] = useState([10,20,30])
 
   function changeA() {
     setA(a + 1);
+    setUsername("Tanisha Kothari");
+    setUsers([...users, a + 1]);
   }
 
   return (
     <div>
+      <h1>Users: {users.join(", ")}</h1>
+      <h1>Value of user is {username}</h1>
       <h1>value of a is {a}</h1>
       <button onClick={changeA}>Click</button>
     </div>
