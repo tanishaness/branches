3e5abc3c0ee31e9ba163f75dea9f47c144e57772
Tanisha Lalwani <145191259+tanishaness@users.noreply.github.com>
2025-11-20 00:25:16 +0530

    two way binding

--- template/src/App.jsx
+++ template/src/App.jsx
@@ -63,9 +63,22 @@ export default App
 import React from 'react'
 
 const App = () => {
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log('form submitted');
+  }
   return (
-    <div>App</div>
+    <div>
+      <form onSubmit={(e) => submitHandler(e)}>
+        <input type="text" placeholder="Enter your name" />
+        
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   )
 }
+
+
 
 export default App
